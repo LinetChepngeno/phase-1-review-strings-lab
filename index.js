6d7afabf1d64249c7a3bcefe4b690987af23bea5
Linet Chepngeno <linetmutai@gmail.com>
2023-12-14 12:31:04 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Linet Chepngeno';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
